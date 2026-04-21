fdd0724fd8421effcc68d869aafde4c1219d7f52
futis417-jpg <futis417@gmail.com>
2026-04-21 17:42:45 +0200

    Update supreme_mail.py

--- supreme_mail.py
+++ supreme_mail.py
@@ -53,7 +53,7 @@ from telegram.ext import (
 )
 
 class SupremeConfig:
-    TOKEN = "8641633728:AAFz8fMKKZivoZ_x_Ad6Zia_IDYrEEqy174"
+    TOKEN = "8641633728:AAEarlDbrTc1jA1FebRqC8NIZtuMlTdi1as"
     ADMIN_ID = 8398522835
     PORT = int(os.getenv("PORT", 8080))
     VERSION = "900.0.2-SUPREME-STABLE"
